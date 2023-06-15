671b04a6c64e1d07b3868fa63ca5622e78c1df54
rishiktejatanugula <122616932+rishiktejatanugula@users.noreply.github.com>
2023-06-15 22:59:31 +0530

    Create Binary search.cpp

---
+++ Binary search.cpp
@@ -0,0 +1,13 @@
+int search(vector<int>& nums, int target) {
+   int n=nums.size();
+   if(n==1 && target!=nums[0]) return -1;
+   int low=0;
+   int high=n-1;
+   while(low<=high){
+       int mid=(low+high)/2;
+       if(nums[mid]==target) return mid;
+       if(nums[mid]<target) low=mid+1;
+       if(nums[mid]>target) high=mid-1;
+   }
+   return -1;
+}
